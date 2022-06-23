eeb8fb6ccb43608e99161dd7bf97323f993ca570
Erik Gillis <erik.gillis@mail.utoronto.ca>
2022-06-23 11:57:26 -0400

    added argument flags, removed mass bin support

--- equilibrium_setup.py
+++ equilibrium_setup.py
@@ -18,16 +18,32 @@ from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 
 import sympy as sym
+import argparse
 
-# Halo number to start from
-nstart = 0
-# Halos to extract
-nmake = 10
-# Pericenter Range
-peri_range = [0, 100]
+parser = argparse.ArgumentParser()
+parser.add_argument("--manual", action="store_true", help="Use manual values in setup.py")
+parser.add_argument("--nstart", type=int, help="Simulation number to start initializing at")
+parser.add_argument("--nmake", type=int, help="Number of simulations to initialize")
+parser.add_argument("--cfactor", default=1, type=int, help="Factor to multiply concentration by")
+parser.add_argument("--kmax", default=None, type=int, help="Manually set kmax value")
+args = parser.parse_args()
 
-# Concentration Factor
-cfactor = 1
+
+if args.manual:
+    # Halo number to start from
+    nstart = 500
+    # Halos to extract
+    nmake = 1
+    # Pericenter Range
+    peri_range = [0, 100]
+    # Concentration Factor
+    cfactor = 1
+
+else:
+    nstart = args.nstart
+    nmake = args.nmake
+    peri_range = [0, 100]
+    cfactor = args.cfactor
 
 ro=8.
 vo=220.
@@ -117,7 +133,7 @@ peri, apo, eccen, energy = np.loadtxt("orbital_params.dat").T[2:]
 
 np.savetxt('idsubs.dat', np.vstack((sim_nums, idsubs)).T, fmt='%06i')
 gyr_command_base = 'gyrfalcON in=%s.nemo out=%s.nemo tstop=%f eps=%f kernel=2 '\
-                + f'step=%f kmax=%i Nlev=5 fac=0.01 > %s.out_z0%s 2>&1'
+                + f'step=%f kmax=%i Nlev=1 fac=0.01 > %s.out_z0%s 2>&1'
 mkhalo_base = "mkhalo out=%s.nemo nbody=%i model=NFW r_s=%f M=%f r_t=-%f WD_units=t"
 
 tstep_restart = (t05-t07)/10.  
@@ -126,97 +142,98 @@ tstep_restart = (t05-t07)/10.
 joblist = open("jobs_to_run.dat", "a")
 finlist = open("finished.dat", "w")
 finlist.close()
-mass_shuffled = m200[idsubs]
+# mass_shuffled = m200[idsubs]
 
-for mass_range in [[1e6, 1e13]]:
-    # Cut all our data arrays
-    # Cut for m200
-    mass_cut = np.logical_and(mass_shuffled >= mass_range[0], mass_shuffled < mass_range[1])
-    # Cut for pericenters
-    peri_cut = np.logical_and(peri >= peri_range[0], peri < peri_range[1])
-    # Now make sure we get the start right 
-    index_cut = sim_nums > nstart
-    # Combine Everything
-    valid_indeces = np.argwhere(np.logical_and(mass_cut, index_cut)).flatten()[:nmake]
-    print(valid_indeces)
+# Cut all our data arrays
+# Cut for pericenters
+peri_cut = np.logical_and(peri >= peri_range[0], peri < peri_range[1])
+# Now make sure we get the start right 
+index_cut = sim_nums > nstart
+# Combine Everything
+valid_indeces = np.argwhere(np.logical_and(index_cut, peri_cut)).flatten()[:nmake]
+print(valid_indeces)
 
-    for i in valid_indeces:
-        indx = idsubs[i]
-        sim_num = sim_nums[i]
+for i in valid_indeces:
+    indx = idsubs[i]
+    sim_num = sim_nums[i]
 
-        # Set Up Subhalo Parameters
-        hz = cosmo.H(zinfall[indx]).value/100
+    # Set Up Subhalo Parameters
+    hz = cosmo.H(zinfall[indx]).value/100
 
-        # Make Directory
-        dirname = "{:06}_{:06}".format(sim_num, indx)
-        os.mkdir(dirname)
-        fname = f"{dirname}/{dirname}"
-        # Open Job file
-        jobfile = open(f"{dirname}/job_commands.sh", "w")
-        # Open a Parameters File
-        params = open(f"{dirname}/{str(sim_num).zfill(6)}_sim_params.dat", "w")
-        param_tup = ("job num", "job id", "start z", "sim start", 
-                     "evo time", "tstep", "radius", "x", "y", "z", 
-                     "vx", "vy", "vz","mass")
-        # Some Fancy Formating to save the information
-        params.write("#"+(("%11s "*len(param_tup)) % param_tup) + "\n")
-        jobfile.write(r"#!/usr/bin/bash" + "\n"*2)
-        jobfile.write("touch running\n")
+    # Make Directory
+    dirname = "{:06}_{:06}".format(sim_num, indx)
+    os.mkdir(dirname)
+    fname = f"{dirname}/{dirname}"
+    # Open Job file
+    jobfile = open(f"{dirname}/job_commands.sh", "w")
+    # Open a Parameters File
+    params = open(f"{dirname}/{str(sim_num).zfill(6)}_sim_params.dat", "w")
+    param_tup = ("job num", "job id", "start z", "sim start", 
+                 "evo time", "tstep", "radius", "x", "y", "z", 
+                 "vx", "vy", "vz","mass")
+    # Some Fancy Formating to save the information
+    params.write("#"+(("%11s "*len(param_tup)) % param_tup) + "\n")
+    jobfile.write(r"#!/usr/bin/bash" + "\n"*2)
+    jobfile.write("touch running\n")
 
-        # Add job to list
-        joblist.write(dirname + "\n")
-        
-        # Will satisfy Van den Bosch criteria
-        epsilon = f(c200[indx])*r200[indx]/(600 * c200[indx]) *\
-                  (10/c200[indx])**0.63
-        Nacc = int(1000.0*(0.01**(-1./0.8))/0.32)
-        
-        tstep = 0.05
-        tfinal = 2
-        zfin = 5
-        gyr_outfile = f'{str(indx).zfill(6)}_z00'
-        
+    # Add job to list
+    joblist.write(dirname + "\n")
+
+    # Will satisfy Van den Bosch criteria
+    epsilon = f(c200[indx])*r200[indx]/(600 * c200[indx]) *\
+              (10/c200[indx])**0.63
+    Nacc = int(1000.0*(0.01**(-1./0.8))/0.32)
+
+    tstep = 0.05
+    tfinal = 2
+    zfin = 5
+    gyr_outfile = f'{str(indx).zfill(6)}_z00'
+
+    if args.kmax is None:
         # Find optimal timestep
-        zmbar = m200[indx]               # In MSun
-        rbar  = r200[indx] * 1000        # In pc
         eps_nbody = epsilon / r200[indx] # Unitless
+        rbar  = r200[indx] * 1000        # In pc
 
-        vbar = 0.06557 * np.sqrt(zmbar / rbar)
+        # Copied from clustertools
+        vbar = 0.06557 * np.sqrt(m200[indx] / rbar)
         tbar = rbar / (1.023*vbar)
 
         tmax_nbody = 0.4 * (Nacc/1e5) * (eps_nbody/0.05)
         tmax_gyrs  = tbar/1000 * tmax_nbody
-        kmax = int(-np.floor(np.log2(tmax_gyrs)))
-        
-        print("Using kmax:", kmax)
-        
-        mkhalo_params = get_mkhalo_params(r200[indx], m200[indx], c200[indx], Nacc, 
-                                          str(indx).zfill(6))
-        print(mkhalo_params)
+        kmax = -np.floor(np.log2(tmax_gyrs))
+        kmax = int(kmax)
+    else:
+        kmax = args.kmax
 
-        mkhalo_command = mkhalo_base % mkhalo_params
-        gyr_command = gyr_command_base % (f'{str(indx).zfill(6)}', gyr_outfile,
-                                          tfinal, epsilon, tstep, kmax, 
-                                          str(indx).zfill(6), str(zfin)) 
+    print("Using kmax:", kmax)
 
-        jobfile.write(mkhalo_command + '\n')
-        jobfile.write(gyr_command + '\n')
-        params.write((' '+'{:11} '*2+'{:11f} '*(len(param_tup)-3)+'{} '+'\n')\
-                     .format(i+1,indx,zinfall[indx],cosmo.age(zinfall[indx])/u.Gyr,tfinal,tstep,
-                             r200[indx], 0, 0, 0, 0, 0, 0, m200[indx]))
+    mkhalo_params = get_mkhalo_params(r200[indx], m200[indx], c200[indx], Nacc, 
+                                      str(indx).zfill(6))
+    print(mkhalo_params)
 
-        # Make sure we have the .dat files we need
-        jobfile.write('for f in *z*.nemo; do s2a $f $f".dat"; done\n')
+    mkhalo_command = mkhalo_base % mkhalo_params
+    gyr_command = gyr_command_base % (f'{str(indx).zfill(6)}', gyr_outfile,
+                                      tfinal, epsilon, tstep, kmax, 
+                                      str(indx).zfill(6), str(zfin)) 
 
-        # Dirty way to keep track of things
-        jobfile.write("rm running && touch done" + "\n"*2) 
+    jobfile.write(mkhalo_command + '\n')
+    jobfile.write(gyr_command + '\n')
+    params.write((' '+'{:11} '*2+'{:11f} '*(len(param_tup)-3)+'{} '+'\n')\
+                 .format(i+1,indx,zinfall[indx],cosmo.age(zinfall[indx])/u.Gyr,tfinal,tstep,
+                         r200[indx], 0, 0, 0, 0, 0, 0, m200[indx]))
 
-        # Close files
-        jobfile.close()
-        params.close()
+    # Make sure we have the .dat files we need
+    jobfile.write('for f in *z*.nemo; do s2a $f $f".dat"; done\n')
 
-        # Make job commands executable 
-        st = os.stat(f"{dirname}/job_commands.sh")
-        os.chmod(f"{dirname}/job_commands.sh", st.st_mode | stat.S_IEXEC)
+    # Dirty way to keep track of things
+    jobfile.write("rm running && touch done" + "\n"*2) 
+
+    # Close files
+    jobfile.close()
+    params.close()
+
+    # Make job commands executable 
+    st = os.stat(f"{dirname}/job_commands.sh")
+    os.chmod(f"{dirname}/job_commands.sh", st.st_mode | stat.S_IEXEC)
 
 joblist.close()
